7b98bfaa15074fd35c9af4f0d0d10934bedc3583
lds <1019505281@qq.com>
2020-10-20 18:21:34 +0800

    整合了redis

--- src/main/java/org/lds/controller/sampleController.java
+++ src/main/java/org/lds/controller/sampleController.java
@@ -1,6 +1,8 @@
 package org.lds.controller;
 
 import org.lds.pojo.User;
+import org.lds.redis.RedisService;
+import org.lds.redis.UserKey;
 import org.lds.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -14,6 +16,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class sampleController {
     @Autowired
     UserService userService;
+    @Autowired
+    RedisService redisService;
     @RequestMapping("/thymeleaf")
     public String thymeleaf(Model model)
     {
@@ -27,5 +31,21 @@ public class sampleController {
         User user = userService.getByIdService(1);
         model.addAttribute("name",user.getName());
         return "hello";
+    }
+    @RequestMapping("/redisget")
+    public String RedisGet(Model model)
+    {
+        User user=redisService.get(UserKey.getById, ""+2, User.class);
+        model.addAttribute("msg",user.getName());
+        return "msg";
+    }
+    @RequestMapping("/redisset")
+    public String RedisSet(Model model)
+    {
+        User user = new User(2,"joson2");
+
+        redisService.set(UserKey.getById,""+user.getId(),user);
+        model.addAttribute("msg","set成功,key:"+user.getId()+"value"+user.getName());
+        return "msg";
     }
 }
---
+++ src/main/java/org/lds/redis/KeyPrefix.java
@@ -0,0 +1,9 @@
+package org.lds.redis;
+
+public interface KeyPrefix {
+
+    public int expireSeconds();
+
+    public String getPrefix();
+
+}
